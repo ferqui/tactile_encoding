ddf15cd178fbd1c07ab2f5ca16d46b63d1a0029a
Simon Mueller-Cleve <s.mueller-cleve@gmx.de>
2022-09-23 15:42:28 +0200

    create MN_playground

---
+++ MN_playground.py
@@ -0,0 +1,11 @@
+'''
+Code to play around with the parameters oof the Mihalas-Nieburg neuron
+'''
+
+import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+from models import Encoder, MN_neuron
+
