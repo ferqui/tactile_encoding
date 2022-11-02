fc77f752ba94b2d1057dc90cb31d840d2f194d79
Fernando M. Quintana <fernando@ini.uzh.ch>
2022-11-02 13:10:16 +0100

    Plot all layers in training

--- auxiliary.py
+++ auxiliary.py
@@ -2,12 +2,21 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_spikes(spikes):
-    _, t, idx = np.where(spikes.cpu().numpy())
+
+def plot_spikes(spikes, idx):
+    t, idx = np.where(spikes[idx].cpu().numpy())
     fig, ax = plt.subplots(figsize=(10, 6))
-    ax.scatter(x = t, y = idx, s=.5)
+    ax.scatter(x=t, y=idx, s=0.5)
 
     return fig
+
+
+def plot_voltages(voltage, idx):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(voltage[idx])
+
+    return fig
+
 
 @torch.no_grad()
 def compute_classification_accuracy(dataset, network, early, device):
@@ -15,34 +24,54 @@ def compute_classification_accuracy(dataset, network, early, device):
     multi_accs = []
     ttc = None
     for x_local, y_local in dataset:
-        x_local, y_local = x_local.to(device, non_blocking=True), y_local.to(device, non_blocking=True)
+        x_local, y_local = x_local.to(device, non_blocking=True), y_local.to(
+            device, non_blocking=True
+        )
 
         for layer in network:
-            if hasattr(layer.__class__, 'reset'):
+            if hasattr(layer.__class__, "reset"):
                 layer.reset()
 
-        spk_hidden = []
-        s_out_rec = []
-        for t in range(x_local.shape[1]):
-            out = network(x_local[:,t])
+        mn_spk = []
+        lif1_spk = []
+        lif2_spk = []
 
-            spk_hidden.append(network[-2].state.S)
-            s_out_rec.append(out)
-        spk_hidden = torch.stack(spk_hidden, dim=1)
-        s_out_rec = torch.stack(s_out_rec, dim=1)
+        mn_mem = []
+        lif1_mem = []
+        lif2_mem = []
+        for t in range(x_local.shape[1]):
+            out = network(x_local[:, t])
+
+            # Get the spikes and voltages from the MN neuron encoder
+            mn_spk.append(network[1].state.spk)
+            mn_mem.append(network[1].state.V)
+
+            # Get the spikes and voltages from the first LIF
+            lif1_spk.append(network[2].state.S)
+            lif1_mem.append(network[2].state.mem)
+
+            # Get the spikes and voltages from the second LIF
+            lif2_spk.append(network[3].state.S)
+            lif2_mem.append(network[3].state.mem)
+        mn_spk = torch.stack(mn_spk, dim=1)
+        mn_mem = torch.stack(mn_mem, dim=1)
+        lif1_spk = torch.stack(lif1_spk, dim=1)
+        lif1_mem = torch.stack(lif1_mem, dim=1)
+        lif2_spk = torch.stack(lif2_spk, dim=1)
+        lif2_mem = torch.stack(lif2_mem, dim=1)
 
         # with output spikes
-        m = torch.sum(s_out_rec, 1)  # sum over time
+        m = torch.sum(lif2_spk, 1)  # sum over time
         _, am = torch.max(m, 1)  # argmax over output units
         # compare to labels
         tmp = np.mean((y_local == am).detach().cpu().numpy())
         accs.append(tmp)
-        
+
         if early:
             accs_early = []
-            for t in range(s_out_rec.shape[1] - 1):
+            for t in range(lif2_spk.shape[1] - 1):
                 # with spiking output layer
-                m_early = torch.sum(s_out_rec[:, :t + 1, :], 1)  # sum over time
+                m_early = torch.sum(lif2_spk[:, : t + 1, :], 1)  # sum over time
                 _, am_early = torch.max(m_early, 1)  # argmax over output units
                 # compare to labels
                 tmp_early = np.mean((y_local == am_early).detach().cpu().numpy())
@@ -50,9 +79,9 @@ def compute_classification_accuracy(dataset, network, early, device):
             multi_accs.append(accs_early)
 
     if early:
-        max_time = int(54*25) #ms
-        time_bin_size = int(1) # ms
-        time = range(0,max_time,time_bin_size)
+        max_time = int(54 * 25)  # ms
+        time_bin_size = int(1)  # ms
+        time = range(0, max_time, time_bin_size)
 
         mean_multi = np.mean(multi_accs, axis=0)
         if np.max(mean_multi) > mean_multi[-1]:
@@ -65,12 +94,12 @@ def compute_classification_accuracy(dataset, network, early, device):
                 try:
                     ttc = time[flattening[0]]
                 except:
-                    ttc = time[-1]    
+                    ttc = time[-1]
             else:
                 # time to classify
                 ttc = time[-1]
         else:
             # time to classify
             ttc = time[np.argmax(mean_multi)]
-    
-    return np.mean(accs), ttc, spk_hidden, s_out_rec
+
+    return np.mean(accs), ttc, mn_spk, lif1_spk, lif2_spk, mn_mem, lif1_mem, lif2_mem
--- training.py
+++ training.py
@@ -12,20 +12,20 @@ import numpy as np
 from datasets import load_data
 from parameters.MN_params import MNparams_dict, INIT_MODE
 from models import Encoder, LIF_neuron, MN_neuron
-from auxiliary import compute_classification_accuracy, plot_spikes
+from auxiliary import compute_classification_accuracy, plot_spikes, plot_voltages
 
 from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset, DataLoader
 
 firing_mode_dict = {
-    'FA':{'a': 5, 'A1': 0, 'A2': 0},
-    'SA':{'a': 0, 'A1': 0, 'A2': 0},
-    'MIX':{'a': 5, 'A1': 5, 'A2': -0.3},
+    "FA": {"a": 5, "A1": 0, "A2": 0},
+    "SA": {"a": 0, "A1": 0, "A2": 0},
+    "MIX": {"a": 5, "A1": 5, "A2": -0.3},
 }
 
+
 def main(args):
-    device = torch.device(
-        'cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     ###########################################
     ##                Dataset                ##
     ###########################################
@@ -35,7 +35,8 @@ def main(args):
     nb_channels = data.shape[-1]
 
     x_train, x_test, y_train, y_test = train_test_split(
-        data, labels, test_size=0.2, shuffle=True, stratify=labels)
+        data, labels, test_size=0.2, shuffle=True, stratify=labels
+    )
 
     ds_train = TensorDataset(x_train, y_train)
     ds_test = TensorDataset(x_test, y_test)
@@ -75,11 +76,28 @@ def main(args):
     # nn.init.normal_(
     #     A2, mean=MNparams_dict[INIT_MODE][2], std=fwd_weight_scale / np.sqrt(nb_inputs))
 
-    network = nn.Sequential(Encoder(nb_inputs, encoder_weight_scale, nb_input_copies),
-                            MN_neuron(nb_inputs, firing_mode_dict[args.firing_mode], train=args.train),
-                            LIF_neuron(nb_inputs, nb_hidden, alpha, beta, is_recurrent=True,
-                                    fwd_weight_scale=fwd_weight_scale, rec_weight_scale=rec_weight_scale),
-                            LIF_neuron(nb_hidden, nb_outputs, alpha, beta, is_recurrent=False, fwd_weight_scale=fwd_weight_scale, rec_weight_scale=rec_weight_scale)).to(device)
+    network = nn.Sequential(
+        Encoder(nb_inputs, encoder_weight_scale, nb_input_copies, ds_min=None, ds_max=None),
+        MN_neuron(nb_inputs, firing_mode_dict[args.firing_mode], train=args.train),
+        LIF_neuron(
+            nb_inputs,
+            nb_hidden,
+            alpha,
+            beta,
+            is_recurrent=True,
+            fwd_weight_scale=fwd_weight_scale,
+            rec_weight_scale=rec_weight_scale,
+        ),
+        LIF_neuron(
+            nb_hidden,
+            nb_outputs,
+            alpha,
+            beta,
+            is_recurrent=False,
+            fwd_weight_scale=fwd_weight_scale,
+            rec_weight_scale=rec_weight_scale,
+        ),
+    ).to(device)
     print(network)
 
     ###########################################
@@ -87,8 +105,7 @@ def main(args):
     ###########################################
     batch_size = 128
 
-    optimizer = torch.optim.Adamax(
-        network.parameters(), lr=0.005, betas=(0.9, 0.995))
+    optimizer = torch.optim.Adamax(network.parameters(), lr=0.005, betas=(0.9, 0.995))
 
     # The log softmax function across output units
     log_softmax_fn = nn.LogSoftmax(dim=1)
@@ -100,47 +117,71 @@ def main(args):
 
     writer = SummaryWriter()  # For logging purpose
 
-    dl_train = DataLoader(ds_train, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-    dl_test = DataLoader(ds_test, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    dl_train = DataLoader(
+        ds_train, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True
+    )
+    dl_test = DataLoader(
+        ds_test, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True
+    )
     pbar = trange(nb_epochs)
     for e in pbar:
         local_loss = []
         accs = []  # accs: mean training accuracies for each batch
         for x_local, y_local in dl_train:
-            x_local, y_local = x_local.to(device, non_blocking=True), y_local.to(device, non_blocking=True)
+            x_local, y_local = x_local.to(device, non_blocking=True), y_local.to(
+                device, non_blocking=True
+            )
 
             # Reset all the layers in the network
             for layer in network:
-                if hasattr(layer.__class__, 'reset'):
+                if hasattr(layer.__class__, "reset"):
                     layer.reset()
 
             # Simulate the network
             # we are going to record the hidden layer
             # spikes for regularization purposes
             loss_local = 0
-            spk_rec = []
-            out_rec = []
-            s_out_rec = []
+
+            mn_spk = []
+            lif1_spk = []
+            lif2_spk = []
+
+            mn_mem = []
+            lif1_mem = []
+            lif2_mem = []
+
             for t in range(x_local.shape[1]):
                 out = network(x_local[:, t])
 
-                # Get the spikes of the hidden layer
-                spk_rec.append(network[-2].state.S)
-                # Get the voltage of the last layer
-                out_rec.append(network[-1].state.mem)
-                s_out_rec.append(out)
-            spk_rec = torch.stack(spk_rec, dim=1)
-            out_rec = torch.stack(out_rec, dim=1)
-            s_out_rec = torch.stack(s_out_rec, dim=1)
+                # Get the spikes and voltages from the MN neuron encoder
+                mn_spk.append(network[1].state.spk)
+                mn_mem.append(network[1].state.V)
 
-            m = torch.sum(s_out_rec, 1)  # sum over time
+                # Get the spikes and voltages from the first LIF
+                lif1_spk.append(network[2].state.S)
+                lif1_mem.append(network[2].state.mem)
+
+                # Get the spikes and voltages from the second LIF
+                lif2_spk.append(network[3].state.S)
+                lif2_mem.append(network[3].state.mem)
+
+            mn_spk = torch.stack(mn_spk, dim=1)
+            mn_mem = torch.stack(mn_mem, dim=1)
+            lif1_spk = torch.stack(lif1_spk, dim=1)
+            lif1_mem = torch.stack(lif1_mem, dim=1)
+            lif2_spk = torch.stack(lif2_spk, dim=1)
+            lif2_mem = torch.stack(lif2_mem, dim=1)
+
+            m = torch.sum(lif2_spk, 1)  # sum over time
             log_p_y = log_softmax_fn(m)
 
             # Here we can set up our regularizer loss
             reg_loss = args.reg_spikes * torch.mean(
-                torch.sum(spk_rec, 1))  # e.g., L1 loss on total number of spikes (original: 1e-3)
+                torch.sum(lif1_spk, 1)
+            )  # e.g., L1 loss on total number of spikes (original: 1e-3)
             reg_loss += args.reg_neurons * torch.mean(
-                torch.sum(torch.sum(spk_rec, dim=0), dim=0) ** 2)  # L2 loss on spikes per neuron (original: 2e-6)
+                torch.sum(torch.sum(lif1_spk, dim=0), dim=0) ** 2
+            )  # L2 loss on spikes per neuron (original: 2e-6)
 
             # Here we combine supervised loss and the regularizer
             loss_val = loss_fn(log_p_y, y_local) + reg_loss
@@ -163,8 +204,16 @@ def main(args):
         accs_hist[0].append(mean_accs)
 
         # Calculate test accuracy in each epoch on the testing dataset
-        test_acc, test_ttc, spk_hidden, spk_output = compute_classification_accuracy(
-            dl_test, network, True, device)
+        (
+            test_acc,
+            test_ttc,
+            mn_spk,
+            lif1_spk,
+            lif2_spk,
+            mn_mem,
+            lif1_mem,
+            lif2_mem,
+        ) = compute_classification_accuracy(dl_test, network, True, device)
         accs_hist[1].append(test_acc)  # only safe best test
         ttc_hist.append(test_ttc)
 
@@ -172,47 +221,72 @@ def main(args):
         ##               Plotting                ##
         ###########################################
 
-        fig1 = plot_spikes(spk_hidden)
-        fig2 = plot_spikes(spk_output)
+        fig1 = plot_spikes(mn_spk)
+        fig2 = plot_spikes(lif1_spk)
+        fig3 = plot_spikes(lif2_spk)
+
+        fig4 = plot_voltages(mn_mem)
+        fig5 = plot_voltages(lif1_mem)
+        fig6 = plot_voltages(lif2_mem)
 
         ###########################################
         ##                Logging                ##
         ###########################################
 
-        writer.add_scalar('Accuracy/test', test_acc, global_step=e)
-        writer.add_scalar('Accuracy/train', mean_accs, global_step=e)
-        writer.add_scalar('Loss', mean_loss, global_step=e)
-        writer.add_figure('Hidden spikes', fig1, global_step=e)
-        writer.add_figure('Output spikes', fig2, global_step=e)
+        writer.add_scalar("Accuracy/test", test_acc, global_step=e)
+        writer.add_scalar("Accuracy/train", mean_accs, global_step=e)
+        writer.add_scalar("Loss", mean_loss, global_step=e)
+        writer.add_figure("MN spikes", fig1, global_step=e)
+        writer.add_figure("LIF1 spikes", fig2, global_step=e)
+        writer.add_figure("LIF2 spikes", fig3, global_step=e)
+        writer.add_figure("MN voltage", fig4, global_step=e)
+        writer.add_figure("LIF1 voltage", fig5, global_step=e)
+        writer.add_figure("LIF2 voltage", fig6, global_step=e)
 
-        pbar.set_postfix_str("Train accuracy: " + str(np.round(accs_hist[0][-1] * 100, 2)) + '%. Test accuracy: ' + str(
-            np.round(accs_hist[1][-1] * 100, 2)) + '%, Loss: ' + str(np.round(mean_loss, 2)))
+        pbar.set_postfix_str(
+            "Train accuracy: "
+            + str(np.round(accs_hist[0][-1] * 100, 2))
+            + "%. Test accuracy: "
+            + str(np.round(accs_hist[1][-1] * 100, 2))
+            + "%, Loss: "
+            + str(np.round(mean_loss, 2))
+        )
+
 
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser('Encoding')
-    parser.add_argument('--firing-mode', type=str, default='FA', choices=['FA', 'SA', 'MIX'], 
-        help="Choose between different firing modes")
-    parser.add_argument('--norm', type=float, default=10.0,
-        help='Data normalization')
-    parser.add_argument('--expansion', type=int, default=1,
-        help='Number of channel expansion (default: 1 (no expansion)).')
-    parser.add_argument('--tau_mem', type=float, default=0.02,
-        help='Membrane time constant.')
-    parser.add_argument('--tau_ratio', type=float, default=2,
-        help='Tau ratio.')
-    parser.add_argument('--fwd_weight_scale', type=float, default=1,
-        help='fwd_weight_scale.')
-    parser.add_argument('--weight_scale_factor', type=float, default=0.01,
-        help='weight_scale_factor')
-    parser.add_argument('--reg_spikes', type=float, default=0.004,
-        help='reg_spikes')
-    parser.add_argument('--reg_neurons', type=float, default=0.000001,
-        help='reg_neurons')
+    parser = argparse.ArgumentParser("Encoding")
+    parser.add_argument(
+        "--firing-mode",
+        type=str,
+        default="FA",
+        choices=["FA", "SA", "MIX"],
+        help="Choose between different firing modes",
+    )
+    parser.add_argument("--norm", type=float, default=10.0, help="Data normalization")
+    parser.add_argument(
+        "--expansion",
+        type=int,
+        default=1,
+        help="Number of channel expansion (default: 1 (no expansion)).",
+    )
+    parser.add_argument(
+        "--tau_mem", type=float, default=0.02, help="Membrane time constant."
+    )
+    parser.add_argument("--tau_ratio", type=float, default=2, help="Tau ratio.")
+    parser.add_argument(
+        "--fwd_weight_scale", type=float, default=1, help="fwd_weight_scale."
+    )
+    parser.add_argument(
+        "--weight_scale_factor", type=float, default=0.01, help="weight_scale_factor"
+    )
+    parser.add_argument("--reg_spikes", type=float, default=0.004, help="reg_spikes")
+    parser.add_argument(
+        "--reg_neurons", type=float, default=0.000001, help="reg_neurons"
+    )
 
-    parser.add_argument('--train', action="store_true",
-        help='Train the MN neuron.')
+    parser.add_argument("--train", action="store_true", help="Train the MN neuron.")
     args = parser.parse_args()
     assert args.expansion > 0, "Expansion number should be greater that 0"
     main(args)
