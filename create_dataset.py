ceaef5c60180bbf1239890e27416f0f047923b5b
Simon Mueller-Cleve <s.mueller-cleve@gmx.de>
2023-01-18 14:25:45 +0100

    TODO fix error in "Phasic spiking"

--- create_dataset.py
+++ create_dataset.py
@@ -6,41 +6,27 @@ import torch
 import torch.nn as nn
 
 from utils.models import MN_neuron
-# from utils.utils import check_cuda, train_test_validation_split
+from utils.utils import check_cuda, train_test_validation_split
 
 
 def main():
+    device = check_cuda()
     # import neuron params
-    from ideal_params import neuron_parameters, input_currents, time_points
+    from ideal_params import neuron_parameters, input_currents, time_points, runtime
 
     classes = neuron_parameters.keys()
     # run the enocding and create training data
     max_trials = 1000
-    time_steps = 100
+    max_time = 1000 # ms
     encoded_data_original = []
     encoded_data = []
     encoded_label = []
     # create training dataset by iterating over neuron params and input currents
     for _, class_name in enumerate(classes):
         # iterate over changes
-        if class_name == 'Spike latency':
-            max_time = 50
-        elif class_name == 'Tonic spiking' or class_name == 'Spike frequency adaptation' or class_name == 'Afterpotentials' or class_name == 'Basal bistability':
-            max_time = 200
-        elif class_name == 'Class 2':
-            max_time = 300
-        elif class_name == 'Threshold variability' or class_name == 'Integrator' or class_name == 'Hyperpolarizing spiking' or class_name == 'Hyperpolarizing bursting':
-            max_time = 400
-        elif class_name == 'Class 1' or class_name == 'Phasic spiking' or class_name == 'Tonic bursting' or class_name == 'Phasic bursting' or class_name == 'Mixed mode':
-            max_time = 500
-        elif class_name == 'Preferred frequency':
-            max_time = 800
-        elif class_name == 'Accommodation' or class_name == 'Rebound spike' or class_name == 'Input bistability' or class_name == 'Rebound burst':
-            max_time = 1000
-        else:
-            print('No correct time found')
+        sim_time = runtime[class_name]
         if len(input_currents[class_name]) > 1:
-            current = np.zeros((max_time, 1))
+            current = np.zeros((sim_time, 1))
             for counter, actual_current in enumerate(input_currents[class_name]):
                 if counter == 0:
                     current[:time_points[class_name][counter]] = actual_current
@@ -51,7 +37,7 @@ def main():
                             [counter-1]:] = actual_current
         else:
             # const current
-            input_current = np.ones((time_steps, 1)) * \
+            input_current = np.ones((sim_time, 1)) * \
                 input_currents[class_name]
         
         # set up MN neuron
@@ -67,10 +53,15 @@ def main():
         output_s = np.stack(output_s)
         encoded_data_original.append(output_s)
 
-        # TODO create 1 sec of input current for all
+        # stack input current trace if input length < 1000ms
+        factor = int((max_time/sim_time)+0.5)
+        if factor > 1:
+            for clone in range(factor):
+                input_current = np.append(input_current, input_current)
+            input_current = input_current[:max_time]
+            input = torch.as_tensor(input_current)
 
         # compute new neuron output
-        input = torch.as_tensor(input_current)
         output_s = []
         for t in range(input.shape[0]):
             out = neurons(input[t])
