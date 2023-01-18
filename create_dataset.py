e610baee45cc8937ae7e2d0b8f724830416e0bdc
Simon Mueller-Cleve <s.mueller-cleve@gmx.de>
2023-01-18 10:30:36 +0100

    created data

--- create_dataset.py
+++ create_dataset.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
+import pickle as pkl
 
 import torch
 import torch.nn as nn
 
 from utils.models import MN_neuron
-from utils.utils import check_cuda, train_test_validation_split
+# from utils.utils import check_cuda, train_test_validation_split
 
 def main():
     # set up neuron parameters and input current
@@ -32,19 +33,28 @@ def main():
 
     # run the enocding and create training data
     max_trials = 1000
+    time_steps = 100
     encoded_data = []
     encoded_label = []
     # create training dataset by iterating over neuron params and input currents
     for counter, params in enumerate(neuron_parameters):
         # extract class name and get the correct input current
         class_name = class_names[counter]
+        # build the different input current profiles
+        if class_name == "class1":
+            current = input_currents[class_name]
+            input_current = np.ones((time_steps, 1)) * current
+        elif class_name == "class2":
+            current = input_currents[class_name]
+            input_current = np.ones((time_steps, 1)) * current
+        else:
+            current = input_currents[class_name]
+            input_current = np.ones((time_steps, 1)) * current
         # neuron_parameter = params[1]
         current = input_currents[class_name]
-        time_steps = 100
-        input_current = np.ones((time_stes, 1)) * current
-
+        input_current = np.ones((time_steps, 1)) * current
         # set up MN neuron
-        neurons = MN_neuron(1, params, dt=(1/100), train=False)
+        neurons = MN_neuron(1, params, dt=1E-3, train=False)
 
         # compute neuron output
         input = torch.as_tensor(input_current)
@@ -62,10 +72,10 @@ def main():
             encoded_label.append(class_name)
     
     # TODO dump neuron output to file
+    with open('./data_encoding', 'wb') as handle:
+        pkl.dump(encoded_data, handle, protocol=pkl.HIGHEST_PROTOCOL)
+    with open('./label_encoding', 'wb') as handle:
+        pkl.dump(encoded_label, handle, protocol=pkl.HIGHEST_PROTOCOL)
 
-    # run the training
-    
-    print("test")
-    
 if __name__ == '__main__':
     main()
