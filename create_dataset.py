e885f2b3bc70ada2336e39834a840320afaf5dd2
Simon Mueller-Cleve <s.mueller-cleve@gmx.de>
2023-01-18 10:49:56 +0100

    setting up all params

--- create_dataset.py
+++ create_dataset.py
@@ -8,9 +8,31 @@ import torch.nn as nn
 from utils.models import MN_neuron
 # from utils.utils import check_cuda, train_test_validation_split
 
+
 def main():
     # set up neuron parameters and input current
-    class_names = ["class1", "class2"]
+    class_names = [
+        "Tonic spiking",
+        "Class 1",
+        "Spike frequency adaptation",
+        "Phasic spiking",
+        "Accommodation",
+        "Threshold variability",
+        "Rebound spike",
+        "Class 2",
+        "Integrator",
+        "Input bistability",
+        "Hyperpolarizing spiking",
+        "Hyperpolarizing bursting",
+        "Tonic bursting",
+        "Phasic bursting",
+        "Rebound burst",
+        "Mixed mode",
+        "Afterpotentials",
+        "Basal bistability",
+        "Preferred frequency",
+        "Spike latency",
+    ]
 
     neuron_parameters = []
 
@@ -27,8 +49,26 @@ def main():
     neuron_parameters.append(class2)
 
     input_currents = {
-        "class1": 1,
-        "class2": 1.3,
+        "Tonic spiking": 1.5,
+        "Class 1": 1.000001, # 1 + 1E-6
+        "Spike frequency adaptation": 2,
+        "Phasic spiking": 1.5,
+        "Accommodation": [1.5, 0, 0.5, 1, 1.5, 0],
+        "Threshold variability": [1.5, 0, -1.5, 0, 1.5, 0],
+        "Rebound spike": [0, -3.5, 0],
+        "Class 2": 2.000002, # 2(1 + 1E-6)
+        "Integrator": [1.5, 0, 1.5, 0, 1.5, 0, 1.5, 0],
+        "Input bistability": [1.5, 1.7, 1.5, 1.7],
+        "Hyperpolarizing spiking": -1,
+        "Hyperpolarizing bursting": -1,
+        "Tonic bursting": 2,
+        "Phasic bursting": 1.5,
+        "Rebound burst": [0, -3.5, 0],
+        "Mixed mode": 2,
+        "Afterpotentials": [2, 0],
+        "Basal bistability": [5, 0, 5, 0],
+        "Preferred frequency": [5, 0, 4, 0, 5, 0, 4, 0],
+        "Spike latency": [8, 0],
     }
 
     # run the enocding and create training data
@@ -70,12 +110,13 @@ def main():
             # store neuron output
             encoded_data.append(output_s)
             encoded_label.append(class_name)
-    
+
     # TODO dump neuron output to file
     with open('./data_encoding', 'wb') as handle:
         pkl.dump(encoded_data, handle, protocol=pkl.HIGHEST_PROTOCOL)
     with open('./label_encoding', 'wb') as handle:
         pkl.dump(encoded_label, handle, protocol=pkl.HIGHEST_PROTOCOL)
+
 
 if __name__ == '__main__':
     main()
