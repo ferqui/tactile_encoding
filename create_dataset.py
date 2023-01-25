176876311f819a12061173bdb4db706c3cd2ecaf
Simon Mueller-Cleve <60852381+2103simon@users.noreply.github.com>
2023-01-25 11:17:06 +0100

    fix error in noise addition

--- create_dataset.py
+++ create_dataset.py
@@ -6,18 +6,15 @@ import torch
 from utils.models import MN_neuron
 
 
-def main():
-    # device = check_cuda()
+def original(add_noise=False, temp_jitter=False):
     # import neuron params
     from ideal_params import neuron_parameters, input_currents, time_points, runtime
 
     classes = neuron_parameters.keys()
+
     # run the enocding and create training data
-    max_trials = 100
-    max_time = 1000 # ms
     encoded_data_original = []
-    encoded_data = []
-    encoded_label = []
+
     # create training dataset by iterating over neuron params and input currents
     for _, class_name in enumerate(classes):
         # do some sanity checks
@@ -35,24 +32,30 @@ def main():
             input_current = np.zeros((sim_time, 1))
             for counter, actual_current in enumerate(input_currents[class_name]):
                 if counter == 0:
-                    input_current[:time_points[class_name][counter]] = actual_current
+                    input_current[:time_points[class_name]
+                                  [counter]] = actual_current
 
                 elif counter < len(time_points[class_name]):
                     input_current[time_points[class_name][counter-1]:time_points[class_name][counter]] = actual_current
 
                 elif counter < len(time_points[class_name]):
                     input_current[time_points[class_name]
-                            [counter-1]:] = actual_current
+                                  [counter-1]:] = actual_current
         else:
             # const current
             input_current = np.ones((sim_time, 1)) * \
                 input_currents[class_name]
-            # input_current = np.ones(sim_time) * \
-            #     input_currents[class_name]
-            # input_current = np.transpose(input_current)
+
         # set up MN neuron
         neurons = MN_neuron(
             1, neuron_parameters[class_name], dt=1E-3, train=False)
+
+        if add_noise:
+            # add noise
+            noise = np.random.normal(
+                loc=0.0, scale=0.1, size=input_current.size)
+            input_current = np.array(
+                [input_current[x] + noise[x] for x in range(len(input_current))])
 
         # compute neuron output
         input = torch.as_tensor(input_current)
@@ -60,45 +63,156 @@ def main():
         output_s = []
         for t in range(input.shape[0]):
             out = neurons(input[t])
-            output_s.append(out[0].cpu().numpy()) # [0] is needed for single neuron
+            # [0] is needed for single neuron
+            output_s.append(out[0].cpu().numpy())
             output_v.append(neurons.state.V[0].cpu().numpy())
-        # output_s = np.stack(output_s)
-        # output_v = np.stack(output_v)
-        encoded_data_original.append([output_s, output_v])
+        encoded_data_original.append([output_s, output_v, input_current])
 
-        # stack input current trace if input length < 1000ms
-        factor = int((max_time/sim_time)) + 1
-        if factor > 1:
-            input_current_list = []
-            for _ in range(factor):
-                input_current_list = np.append(input_current_list, input_current)
-            input_current = np.array(input_current_list[:max_time])
-            if len(input_current) != 1000:
-                print("ERROR")
+    filename = './data/data_encoding_original'
+    if add_noise:
+        filename = filename + '_noisy'
+    if temp_jitter:
+        filename = filename + '_temp_jitter'
+
+    # dump neuron output to file
+    with open(filename, 'wb') as handle:
+        pkl.dump(encoded_data_original, handle,
+                    protocol=pkl.HIGHEST_PROTOCOL)
+
+
+def fix_time(add_noise=False, temp_jitter=False):
+    # import neuron params
+    from ideal_params import neuron_parameters, input_currents, time_points, runtime
+
+    classes = neuron_parameters.keys()
+
+    # run the enocding and create training data
+    max_trials = 100
+    max_time = 1000  # ms
+    encoded_data = []
+    encoded_label = []
+
+    # create training dataset by iterating over neuron params and input currents
+    for _, class_name in enumerate(classes):
+        print(f'Working on {class_name}')
+        # do some sanity checks
+        if runtime[class_name] is None:
+            print('No runtime given.')
+        if input_currents[class_name] is None:
+            print('No input current given.')
+
+        # create max_trials trials per class
+        for _ in range(max_trials):
+            # iterate over changes
+            sim_time = runtime[class_name]
+            # variable input currents over time
+            if len(input_currents[class_name]) > 1:
+                if time_points[class_name] is None:
+                    print('Missing time points.')
+                input_current = np.zeros((sim_time, 1))
+                for counter, actual_current in enumerate(input_currents[class_name]):
+                    if counter == 0:
+                        input_current[:time_points[class_name]
+                                      [counter]] = actual_current
+
+                    elif counter < len(time_points[class_name]):
+                        input_current[time_points[class_name][counter-1]
+                            :time_points[class_name][counter]] = actual_current
+
+                    elif counter < len(time_points[class_name]):
+                        input_current[time_points[class_name]
+                                      [counter-1]:] = actual_current
+            else:
+                # const current
+                input_current = np.ones((sim_time, 1)) * \
+                    input_currents[class_name]
+
+            # set up MN neuron
+            neurons = MN_neuron(
+                1, neuron_parameters[class_name], dt=1E-3, train=False)
+
+            # stack input current trace if input length < 1000ms
+            # round down, with first list initialized
+            factor = round((max_time/sim_time)-0.5)
+            if factor > 1:
+                if add_noise:
+                    noise = np.random.normal(
+                        loc=0.0, scale=0.1, size=input_current.size)
+                    input_current_list = np.array(
+                        [input_current[x] + noise[x] for x in range(len(input_current))])
+                else:
+                    input_current_list = input_current
+                for _ in range(factor):
+                    if add_noise:
+                        noise = np.random.normal(
+                            loc=0.0, scale=0.1, size=input_current.size)
+                        input_current_list = np.append(input_current_list, np.array(
+                            [input_current[x] + noise[x] for x in range(len(input_current))]), axis=0)
+                    else:
+                        input_current_list = np.append(
+                            input_current_list, input_current, axis=0)
+
+                # cut down to fix length
+                input_current = np.array(input_current_list[:max_time])
+                if len(input_current) != 1000:
+                    print("ERROR")
+            else:
+                if add_noise:
+                    noise = np.random.normal(
+                        loc=0.0, scale=0.1, size=input_current.size)
+                    input_current = np.array(
+                        [input_current[x] + noise[x] for x in range(len(input_current))])
+
             input = torch.as_tensor(input_current)
 
             # compute new neuron output
+            output_v = []
             output_s = []
             for t in range(input.shape[0]):
                 out = neurons(input[t])
+                # [0] is needed for single neuron
                 output_s.append(out[0].cpu().numpy())
-            # output_s = np.stack(output_s)
-            
-        # create max_trials trials per class
-        for _ in range(max_trials):
+                output_v.append(neurons.state.V[0].cpu().numpy())
+
             # store neuron output
-            encoded_data.append(output_s)
+            encoded_data.append([output_s, output_v, input_current])
             encoded_label.append(class_name)
 
-    print(np.array(encoded_data).shape)
+    filename = './data/data_encoding'
+    if add_noise:
+        filename = filename + '_noisy'
+    if temp_jitter:
+        filename = filename + '_temp_jitter'
+
     # dump neuron output to file
-    with open('./data_encoding_original', 'wb') as handle:
-        pkl.dump(encoded_data_original, handle, protocol=pkl.HIGHEST_PROTOCOL)
-    with open('./data_encoding', 'wb') as handle:
+    with open(filename, 'wb') as handle:
         pkl.dump(encoded_data, handle, protocol=pkl.HIGHEST_PROTOCOL)
     with open('./label_encoding', 'wb') as handle:
         pkl.dump(encoded_label, handle, protocol=pkl.HIGHEST_PROTOCOL)
 
 
 if __name__ == '__main__':
-    main()
+    # original length
+    print('Creating original data.')
+    original()
+
+    print('Creating noisy original data.')
+    original(add_noise=True, temp_jitter=False)
+
+    # TODO implement temporal jitter
+    # print('Creating noisy original data with temporal jitter.')
+    # original(add_noise=True, temp_jitter=True)
+
+    # fix 1000ms length
+    print('Creating 1000ms data.')
+    fix_time()
+
+    print('Creating noisy 1000ms data.')
+    fix_time(add_noise=True, temp_jitter=False)
+
+    # TODO implement temporal jitter
+    # print('Creating noisy 1000ms data with temporal jitter.')
+    # fix_time(add_noise=True, temp_jitter=True)
+
+
+    print('Finished with data creation.')
