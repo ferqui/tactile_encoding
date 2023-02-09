152466c29daf90b9d5d88039f5670c960308c010
Michele Mastella <m.mastella@rug.nl>
2023-02-09 23:08:42 +0100

    Implemented fitting function for a differential, not sota yet

--- fitting_analysis.py
+++ fitting_analysis.py
@@ -18,12 +18,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 # from classifiers import MahalanobisClassifier
 from scipy.optimize import curve_fit
+#
+# def func(x  ,t, a, b, c,d):
+#     return a*np.exp(b*x) + c/np.log(d*x)
 
-def func(x,t, a, b, c,d):
-    return a*np.exp(b*x) + c/np.log(d*x)
-def func_ode(t,a,b,c,d):
-    tspan = np.hstack([[0],np.hstack([t])])
-    return scint.odeint(func_1,0, tspan, args=(a,b,c,d))[1:,0]
+y0 = 0
+
+
+# def func(time,b,c,x_local_full,e):
+#
+#     return scint.odeint(pend, y0, time, args=(b, c, x_local_full))[1:,0]
+# def func_ode(t,a,b,c,d):
+#     tspan = np.hstack([[0],np.hstack([t])])
+#     return scint.odeint(func,0, tspan, args=(a,b,c,d))[1:,0]
+
+# sol = scint.odeint(pend, y0, t, args=(b, c))
+
+
+# popt, pcov = curve_fit(f, t, sol[:,0], p0=guess)
+
 torch.manual_seed(0)
 np.random.seed(19)
 ##########################################################
@@ -40,7 +53,22 @@ class_labels = dict(zip(list(np.arange(len(MNclass_to_param.keys()))),
                         MNclass_to_param.keys()))
 inv_class_labels = {v: k for k, v in class_labels.items()}
 
+class fitClass:
 
+    def __init__(self,stim):
+        self.stim = stim
+        self.b = 1
+        self.c = 1
+        pass
+    def pend(self,y, t, b, c):
+        dydt = -b * np.exp(c*y) + self.stim[int(t)]
+        return dydt
+    def func(self,time,b,c):
+        # print(time)
+
+        tspan = np.hstack([[0], np.hstack([time])])
+
+        return scint.odeint(self.pend, y0, tspan, args=(b, c))[1:,0]
 ############################################################
 
 def main(args):
@@ -69,6 +97,7 @@ def main(args):
     dict_spk_rec = dict.fromkeys(list_classes, [])
     dict_mem_rec = dict.fromkeys(list_classes, [])
     for MN_class_type in list_classes:
+        x_local_full = []
         neurons = MN_neuron(len(amplitudes) * n_repetitions, MNclass_to_param[MN_class_type], dt=args.dt, train=False)
 
         x_local, list_mean_current = get_input_step_current(dt_sec=args.dt, stim_length_sec=args.stim_length_sec,
@@ -94,6 +123,7 @@ def main(args):
         isi_tensor_list = []
         time_list = []
         x_local_sampled = torch.zeros_like(x_local)
+        x_local_full = x_local.flatten()
         isi_tensor = torch.zeros_like(x_local)
         fig1, axis1 = plt.subplots(nrows=2, ncols=1,sharex=True)
         eee = plt.get_cmap('inferno')
@@ -123,7 +153,17 @@ def main(args):
         time_tensor = torch.concat(time_list)
         x_local_sampled = torch.concat(x_local_sampled_list)
         isi_tensor = torch.concat(isi_tensor_list)
-        popt, pcov = curve_fit(func, x_local_sampled, isi_tensor)
+        b = 1
+        c = 1
+        d = x_local_sampled.numpy()
+        e = time_tensor
+        time = np.linspace(0,len(x_local_full)-1,len(x_local_full))
+        padding = np.zeros_like(x_local_full)
+        padding[time_tensor] = isi_tensor
+        myfit = fitClass(x_local_full.numpy())
+        popt, pcov = curve_fit(myfit.func, time, padding)
+        print('we')
+        # popt, pcov = curve_fit(f,y0, sol[], isi_tensor)
         # plt.plot(time_list, func(x_local_sampled, *popt), 'r',label='fit')
         # plt.plot(time_list,x_local_sampled,'b',label='input')
         # label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
@@ -135,12 +175,13 @@ def main(args):
         plt.plot(time_list[0], isi_tensor_list[0], 'r*-', label='data')
         #mystr = str(np.round(popt[0])) + "exp(" + str(np.round(popt[1],2)) + "x)  + "+str(np.round(popt[2],2)) + "*1/ln("+str(np.round(popt[4],2)) + ")"
         mystr = ''
-        plt.plot(time_list[0], func(x_local_sampled_list[0], *popt), 'gx-', label=mystr)
+        # plt.plot(time_list[0], func(x_local_sampled_list[0], *popt), 'gx-', label=mystr)
+        plt.plot(time, myfit.func(time, *popt), 'gx-')
+
         for i in range(len(isi_tensor_list))[1:]:
 
             plt.plot(time_list[i], x_local_sampled_list[i], 'b+-')
             plt.plot(time_list[i], isi_tensor_list[i], 'r*-')
-            plt.plot(time_list[i], func(x_local_sampled_list[i], *popt), 'gx-')
         plt.legend()
         # plt.legend()
     plt.show()
