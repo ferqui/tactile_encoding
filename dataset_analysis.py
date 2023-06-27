86a9de063a49af904ae9af68a067619b4001fe86
p308783 <n.risi@rug.nl>
2023-06-27 10:17:18 +0200

    Update dataset analysis

--- dataset_analysis.py
+++ dataset_analysis.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import torch
 import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
@@ -6,7 +7,7 @@ import argparse
 import matplotlib.pyplot as plt
 from pathlib import Path
 from tqdm import trange
-
+import seaborn as sns
 import numpy as np
 
 from datasets import load_data
@@ -16,6 +17,7 @@ from auxiliary import compute_classification_accuracy, plot_spikes, plot_voltage
 
 from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset, DataLoader
+from numpy.fft import rfft, rfftfreq
 
 firing_mode_dict = {
     "FA": {"a": 5, "A1": 0, "A2": 0},
@@ -48,6 +50,8 @@ def main(args):
 
     # Find the first occurrence of the unique values in the list of classes (one sample for each letter)
     idx_letters, indices = np.unique(labels, return_index=True)
+
+    # Amplitude:
     n_rows = 4
     n_cols = 7
     fig, axs = plt.subplots(n_rows, n_cols, sharey=True, sharex=True)
@@ -68,9 +72,138 @@ def main(args):
     axs[n_rows - 1, n_cols - 1].set_axis_off()
     fig.set_size_inches(10, 8)
     fig.subplots_adjust(wspace=0.3, hspace=0.3, right=0.8)
-    fig.savefig(folder_fig.joinpath('Samples.pdf'), format='pdf', dpi=300)
-    plt.show()
-    print('Hello')
+    fig.savefig(folder_fig.joinpath('Amplitudes.pdf'), format='pdf', dpi=300)
+
+    # ------------------------------------------------------------------------------------------ Frequency:
+    fig, axs = plt.subplots(n_rows, n_cols, sharey=True, sharex=True)
+    n_samples = data.shape[1]  # number of time steps
+    for i, idx_to_plot in zip(idx_letters, indices):
+        ax = axs[i // n_cols, i % n_cols]
+        x = data[idx_to_plot, :, :]
+        xf = rfftfreq(n_samples, dt)
+        yf = rfft(x, axis=0)
+
+        n_channels = yf.shape[1]
+        n_freq = yf.shape[0]
+        freq = np.array([[i]*n_channels for i in xf]).flatten()
+        yf = np.abs(yf).flatten()
+        ch = np.repeat(np.array([np.arange(n_channels)]), repeats=n_freq, axis=0).flatten()
+        df = pd.DataFrame({'freq': freq, 'y': yf, 'ch': ch})
+        sns.lineplot(data=df, x='freq', y='y',hue='ch', ax=ax)
+        ax.set_title(i)
+
+        if i == (len(idx_letters)-1):
+            sns.move_legend(ax, "right", bbox_to_anchor=(2.5, 0.5))
+        else:
+            ax.get_legend().remove()
+
+    for i in range(n_rows):
+        axs[i, 0].set_ylabel('|yf|')
+
+    for i in range(n_cols):
+        axs[n_rows - 1, i].set_xlabel('Freq (Hz)')
+
+    axs[n_rows - 1, n_cols - 1].set_axis_off()
+    fig.set_size_inches(10, 8)
+    fig.subplots_adjust(wspace=0.3, hspace=0.3, right=0.8)
+    fig.savefig(folder_fig.joinpath('Frequency.pdf'), format='pdf', dpi=300)
+
+    # ------------------------------------------------------------------------------------------ Frequency - zoom in :
+    fig, axs = plt.subplots(n_rows, n_cols, sharey=True, sharex=True)
+    n_samples = data.shape[1]  # number of time steps
+    for i, idx_to_plot in zip(idx_letters, indices):
+        ax = axs[i // n_cols, i % n_cols]
+
+        x = data[idx_to_plot, :, :]
+        xf = rfftfreq(n_samples, dt)
+        yf = rfft(x, axis=0)
+        n_channels = yf.shape[1]
+        n_freq = yf.shape[0]
+        freq = np.array([[i]*n_channels for i in xf]).flatten()
+        yf = np.abs(yf).flatten()
+        ch = np.repeat(np.array([np.arange(n_channels)]), repeats=n_freq, axis=0).flatten()
+
+        df = pd.DataFrame({'freq': freq, 'y': yf, 'ch': ch})
+        sns.lineplot(data=df, x='freq', y='y',hue='ch', ax=ax)
+        ax.set_title(i)
+
+        if i == (len(idx_letters)-1):
+            sns.move_legend(ax, "right", bbox_to_anchor=(2.5, 0.5))
+        else:
+            ax.get_legend().remove()
+
+    for i in range(n_rows):
+        axs[i, 0].set_ylabel('|yf|')
+
+    for i in range(n_cols):
+        axs[n_rows - 1, i].set_xlabel('Freq (Hz)')
+
+    axs[n_rows - 1, n_cols - 1].set_axis_off()
+    ax.set_xlim(0,10)
+    fig.set_size_inches(10, 8)
+    fig.subplots_adjust(wspace=0.3, hspace=0.3, right=0.8)
+    fig.savefig(folder_fig.joinpath('Frequency_zoomed_in.pdf'), format='pdf', dpi=300)
+
+    # ------------------------------------------------------------------------------------------ Slope :
+    fig, axs = plt.subplots(n_rows, n_cols, sharey=True, sharex=True)
+    n_samples = data.shape[1]  # number of time steps
+    for i, idx_to_plot in zip(idx_letters, indices):
+        ax = axs[i // n_cols, i % n_cols]
+
+        x = data[idx_to_plot, :, :]
+        dy_dx = np.diff(x, axis=0)/dt
+        n_channels = x.shape[1]
+        time_bins = np.array([[i]*n_channels for i in range(n_samples-1)]).flatten()
+        dy_dx = dy_dx.flatten()
+        ch = np.repeat(np.array([np.arange(n_channels)]), repeats=n_samples-1, axis=0).flatten()
+        df = pd.DataFrame({'Time bins': time_bins, 'dy_dx': dy_dx, 'ch': ch})
+
+        sns.lineplot(data=df, x='Time bins', y='dy_dx', hue='ch', ax=ax)
+        ax.set_title(i)
+
+        if i == (len(idx_letters)-1):
+            sns.move_legend(ax, "right", bbox_to_anchor=(2.5, 0.5))
+        else:
+            ax.get_legend().remove()
+
+    for i in range(n_rows):
+        axs[i, 0].set_ylabel('dy/dx')
+
+    for i in range(n_cols):
+        axs[n_rows - 1, i].set_xlabel('Time bins')
+
+    axs[n_rows - 1, n_cols - 1].set_axis_off()
+    fig.set_size_inches(10, 8)
+    fig.subplots_adjust(wspace=0.3, hspace=0.3, right=0.8)
+    fig.savefig(folder_fig.joinpath('Slopes.pdf'), format='pdf', dpi=300)
+
+    # ------------------------------------------------------------------------------------------ Slope distribution:
+    fig, axs = plt.subplots(n_rows, n_cols, sharey=True, sharex=True)
+    n_samples = data.shape[1]  # number of time steps
+    for i, idx_to_plot in zip(idx_letters, indices):
+        ax = axs[i // n_cols, i % n_cols]
+
+        x = data[idx_to_plot, :, :]
+        dy_dx = np.diff(x, axis=0)/dt
+        n_channels = x.shape[1]
+        time_bins = np.array([[i]*n_channels for i in range(n_samples-1)]).flatten()
+        dy_dx = dy_dx.flatten()
+        ch = np.repeat(np.array([np.arange(n_channels)]), repeats=n_samples-1, axis=0).flatten()
+        df = pd.DataFrame({'Time bins': time_bins, 'dy_dx': dy_dx, 'ch': ch})
+
+        sns.histplot(data=df, x='dy_dx', ax=ax)
+        ax.set_title(i)
+
+    for i in range(n_rows):
+        axs[i, 0].set_ylabel('Count')
+
+    for i in range(n_cols):
+        axs[n_rows - 1, i].set_xlabel('Slopes')
+
+    axs[n_rows - 1, n_cols - 1].set_axis_off()
+    fig.set_size_inches(10, 8)
+    fig.subplots_adjust(wspace=0.3, hspace=0.3, right=0.8)
+    fig.savefig(folder_fig.joinpath('Slopes_dist.pdf'), format='pdf', dpi=300)
 
 
 if __name__ == "__main__":
