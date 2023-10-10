0417c23392a44d50e08ea683d40461a49df617c9
p308783 <n.risi@rug.nl>
2023-10-10 11:00:35 +0200

    Add commands to generate inputs of type frequency current and amplitude

--- run_generate_MNIST_dataset.py
+++ run_generate_MNIST_dataset.py
@@ -6,10 +6,37 @@ import tqdm
 
 
 def main(args):
+
     if args.data_type == 'frequency':
+        centers = np.linspace(10, 20, args.n_samples_sweep)
+        spans = np.linspace(10, 20, args.n_samples_sweep)
+        sweep = tqdm.tqdm(total=len(centers) * len(spans), desc=f"{args.data_type} Sweeping", position=0, leave=True)
+
+        for center in centers:
+            for span in spans:
+                cmd = (f'python3 generate_MNIST_dataset.py --data_type {args.data_type} '
+                       f'--center {center} --span {span} --n_samples_train {args.n_samples_train} '
+                       f'--sample_size {args.sample_size} --home_dataset {args.home_dataset}')
+                os.system(cmd)
+                sweep.update()
+
+    elif args.data_type == 'amplitude':
+        centers = np.linspace(0.5, 100, args.n_samples_sweep)
+        spans = np.linspace(0.5, 10, args.n_samples_sweep)
+        sweep = tqdm.tqdm(total=len(centers) * len(spans), desc=f"{args.data_type} Sweeping", position=0, leave=True)
+
+        for center in centers:
+            for span in spans:
+                cmd = (f'python3 generate_MNIST_dataset.py --data_type {args.data_type} '
+                       f'--center {center} --span {span} --n_samples_train {args.n_samples_train} '
+                       f'--sample_size {args.sample_size} --home_dataset {args.home_dataset}')
+                os.system(cmd)
+                sweep.update()
+
+    elif args.data_type == 'slope':
         centers = np.linspace(0.5, 5, args.n_samples_sweep)
         spans = np.linspace(0.5, 1, args.n_samples_sweep)
-        sweep = tqdm.tqdm(total=len(centers) * len(spans), desc=f"Frequency Sweeping", position=0, leave=True)
+        sweep = tqdm.tqdm(total=len(centers) * len(spans), desc=f"{args.data_type} Sweeping", position=0, leave=True)
 
         for center in centers:
             for span in spans:
@@ -23,11 +50,9 @@ def main(args):
         cmd = (f'python3 generate_MNIST_dataset.py --data_type {args.data_type} '
                f'--n_samples_train {args.n_samples_train} --home_dataset {args.home_dataset}')
         os.system(cmd)
-    else:
-        # TODO:
-        print('Not implemented yet')
-        pass
 
+    else:
+        pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('dataloader')
@@ -38,14 +63,14 @@ if __name__ == "__main__":
                         choices=['current', 'frequency', 'amplitude', 'slope'])
     parser.add_argument('--n_samples_train',
                         type=int,
-                        default=-1)
+                        default=20000)
     parser.add_argument('--sample_size',
                         type=int,
                         default=10)
     parser.add_argument('--home_dataset',
                         type=str,
                         help='Absolute path to output folder where the dataset is stored',
-                        default='./dataset/')
+                        default='/media/p308783/bics/Nicoletta/tactile_encoding/')
     parser.add_argument('--n_samples_sweep',
                         type=int,
                         default=5)
