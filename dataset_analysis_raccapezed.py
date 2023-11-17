cf98b54494e876cb4c4fd52d38fceebff70902d5
Michele Mastella <m.mastella@rug.nl>
2023-11-17 17:07:57 +0100

    2 to 100

--- dataset_analysis_raccapezed.py
+++ dataset_analysis_raccapezed.py
@@ -797,7 +797,7 @@ if __name__ == "__main__":
     parser.add_argument('--dataset', type=str, default='Braille,MNIST,MNIST_compressed')
     parser.add_argument('--analysis', type=str, default='amplitude,frequency,slope')
     parser.add_argument('--batch_size',  type=int, default=64)
-    parser.add_argument('--epochs_n',  type=int, default=2)
+    parser.add_argument('--epochs_n',  type=int, default=100)
     parser.add_argument('--bins',  type=int, default=10)
     parser.add_argument('--sim_id',  type=int, default=-1)
     parser.add_argument('--stim_len_sec',  type=int, default=300)
